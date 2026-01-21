b20056302cbc6d75a5c0de2c533c1ca35990ce7c
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-21 13:53:32 +0100

    Ajoutez des fichiers projet.

---
+++ Demo-ASP-MVC-01/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_ASP_MVC_01.Controllers
+{
+    public class BlogController : Controller
+    {
+        public string Index(string article)
+        {
+            return $"Choix de l'article : {article}";
+        }
+    }
+}
