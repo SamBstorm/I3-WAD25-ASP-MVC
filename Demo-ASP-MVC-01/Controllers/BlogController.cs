20fd6e3bced491b2bb6fc2cfd38f0de168e8c8a9
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-26 13:47:35 +0100

    Add Demo Razor

--- Demo-ASP-MVC-01/Controllers/BlogController.cs
+++ Demo-ASP-MVC-01/Controllers/BlogController.cs
@@ -12,11 +12,19 @@ namespace Demo_ASP_MVC_01.Controllers
             _logger.Log(LogLevel.Information, "Inticiation du BlogController");
         }
 
+        /* Action Index sans vue
+         
         public string Index(string article)
         {
             _logger.Log(LogLevel.Warning, "Exécution de l'action Index");
             _logger.Log(LogLevel.Error, article);
             return $"Choix de l'article : {article}";
+        }*/
+        public IActionResult Index(string article)
+        {
+            _logger.Log(LogLevel.Warning, "Exécution de l'action Index");
+            _logger.Log(LogLevel.Error, $"Choix de l'article : {article}");
+            return View();
         }
     }
 }
