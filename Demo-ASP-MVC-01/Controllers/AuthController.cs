79016432d679024238b165370c0fa84028b75a23
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-27 14:36:25 +0100

    Add Demo Formulaire : ModelState

--- Demo-ASP-MVC-01/Controllers/AuthController.cs
+++ Demo-ASP-MVC-01/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using Demo_ASP_MVC_01.Models.Auth;
+﻿using Demo_ASP_MVC_01.Handlers;
+using Demo_ASP_MVC_01.Models.Auth;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace Demo_ASP_MVC_01.Controllers
 {
@@ -21,7 +23,35 @@ namespace Demo_ASP_MVC_01.Controllers
         //public IActionResult Login(string mail, string pwd)   // paramètres spécifiques => Récupération des champs d'un formulaire en effectuant la conversion des données (attention au typage et aux attributs name des inputs)
         public IActionResult Login(LoginForm form)
         {
-            return View();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(form.Mail)) ModelState.AddModelError(nameof(form.Mail), "Votre Email ne peut être null, vide ou composé d'espace blanc.");
+                if (!Regex.IsMatch(form.Mail, "^[a-zA-Z0-9\\-\\.=+*$]+@[a-zA-Z0-9\\-\\.=+*$]+$")) ModelState.AddModelError(nameof(form.Mail), "Votre Email ne n'est pas au bon format.");
+                if (form.Pwd.Length < 8 || form.Pwd.Length > 64) ModelState.AddModelError(nameof(form.Pwd), "Le mot de passe doit être compris entre 8 à 64 caractères.");
+                //if (!Regex.IsMatch(form.Pwd, "[a-z]")) ModelState.AddModelError(nameof(form.Pwd), "Le mot de passe doit avoir au minimum un caractère minuscule.");
+                //if (!Regex.IsMatch(form.Pwd, "[A-Z]")) ModelState.AddModelError(nameof(form.Pwd), "Le mot de passe doit avoir au minimum un caractère majuscule.");
+                //if (!Regex.IsMatch(form.Pwd, "[0-9]")) ModelState.AddModelError(nameof(form.Pwd), "Le mot de passe doit avoir au minimum un caractère numérique.");
+                //if (!Regex.IsMatch(form.Pwd, @"[\-=\.@\\/$#]")) ModelState.AddModelError(nameof(form.Pwd), "Le mot de passe doit avoir au minimum un caractère de type symbole.");
+                ModelState.ContainsLowerCharacterValidator(nameof(form.Pwd), form.Pwd);
+                ModelState.ContainsUpperCharacterValidator(nameof(form.Pwd), form.Pwd);
+                ModelState.ContainsNumberValidator(nameof(form.Pwd), form.Pwd);
+                ModelState.ContainsSymbolCharacterValidator(nameof(form.Pwd), form.Pwd);
+                if (!ModelState.IsValid)
+                {
+                    throw new InvalidOperationException("Le model state n'est pas valide.");
+                }
+                //Intérroger la DB
+
+                TempData["message"] = "Connexion réussie";
+                TempData["alert-type"] = "alert-success";
+                return RedirectToAction("Login");
+            }
+            catch (InvalidOperationException ex)
+            {
+                TempData["message"] = "Connexion échouée";
+                TempData["alert-type"] = "alert-warning";
+                return View();
+            }
         }
     }
 }
---
+++ Demo-ASP-MVC-01/Handlers/ModelStateExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.RegularExpressions;
+
+namespace Demo_ASP_MVC_01.Handlers
+{
+    public static class ModelStateExtensions
+    {
+        public static void RegexValidator(
+            this ModelStateDictionary modelState,
+            string key, 
+            object? value, 
+            string errorMessage, 
+            string pattern)
+        {
+            if(!Regex.IsMatch(value.ToString(), pattern))
+            {
+                modelState.AddModelError(key, errorMessage);
+            }
+        }
+
+        public static void ContainsNumberValidator(
+            this ModelStateDictionary modelState,
+            string key,
+            object? value,
+            string errorMessage = "Le champ ne contient pas de nombre.")
+        {
+            modelState.RegexValidator(key, value, errorMessage, "[0-9]");
+        }
+
+        public static void ContainsLowerCharacterValidator(
+            this ModelStateDictionary modelState,
+            string key,
+            object? value,
+            string errorMessage = "Le champ ne contient pas de minuscule.")
+        {
+            modelState.RegexValidator(key, value, errorMessage, "[a-z]");
+        }
+        public static void ContainsUpperCharacterValidator(
+            this ModelStateDictionary modelState,
+            string key,
+            object? value,
+            string errorMessage = "Le champ ne contient pas de majuscule.")
+        {
+            modelState.RegexValidator(key, value, errorMessage, "[A-Z]");
+        }
+
+        public static void ContainsSymbolCharacterValidator(
+            this ModelStateDictionary modelState,
+            string key,
+            object? value,
+            string errorMessage = "Le champ ne contient pas de symbole.")
+        {
+            modelState.RegexValidator(key, value, errorMessage, @"[\-=\.@\\/$#]");
+        }
+    }
+}
