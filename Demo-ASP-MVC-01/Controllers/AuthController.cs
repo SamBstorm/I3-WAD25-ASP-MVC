c7d345409999e4fb258230b82fefbea2e4602515
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-27 12:10:04 +0100

    Add AuthController - Démo Formulaire

---
+++ Demo-ASP-MVC-01/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Demo_ASP_MVC_01.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_ASP_MVC_01.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        //public IActionResult Login(IFormCollection form)      // IFormCollection => Récupération d'un formulaire adaptatif, mais l'ensemble des données sont au format StringValues
+        //public IActionResult Login(string mail, string pwd)   // paramètres spécifiques => Récupération des champs d'un formulaire en effectuant la conversion des données (attention au typage et aux attributs name des inputs)
+        public IActionResult Login(LoginForm form)
+        {
+            return View();
+        }
+    }
+}
---
+++ Demo-ASP-MVC-01/Models/Auth/LoginForm.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo_ASP_MVC_01.Models.Auth
+{
+    public class LoginForm
+    {
+        public string Mail { get; set; }
+        public string Pwd { get; set; }
+    }
+}
