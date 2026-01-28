51b4576ef1d7239df8926b8d001834db1e91b95f
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-28 13:59:35 +0100

    Add Demo-HtmlHelper

--- Demo-ASP-MVC-01/Models/Auth/LoginForm.cs
+++ Demo-ASP-MVC-01/Models/Auth/LoginForm.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Demo_ASP_MVC_01.Models.Auth
 {
     public class LoginForm
     {
+        [DisplayName("Identifiant : ")]
         [Required(ErrorMessage = "L'e-mail est obligatoire.")]
         [EmailAddress(ErrorMessage = "Le format n'est pas valide.")]
         public string Mail { get; set; }
+        [DisplayName("Mot de passe : ")]
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
         [MinLength(8, ErrorMessage = "La taille minimale du mot de passe est de 8 caractères.")]
         [MaxLength(64, ErrorMessage = "La taille maximale du mot de passe est de 64 caractères.")]
